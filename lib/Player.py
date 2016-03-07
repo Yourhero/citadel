26f727eb7bc7052f9544281ca743e85cb48a4c85
thothlogos <naughton.joshua@gmail.com>
2016-03-06 20:57:45 -0800

    movement merge fix

--- lib/Player.py
+++ lib/Player.py
@@ -20,6 +20,25 @@ class Player(Entity):
   def client_update(self):
     self.rect.x += self.vel_x
     self.rect.y += self.vel_y
+    if self.face == 0:
+      self.image = pygame.transform.rotate(self.base_image, 90)
+    elif self.face == 1:
+      self.image = pygame.transform.rotate(self.base_image, 45)
+    elif self.face == 2:
+      self.image = self.base_image
+    elif self.face == 3:
+      self.image = pygame.transform.rotate(self.base_image, 315)
+    elif self.face == 4:
+      self.image = pygame.transform.rotate(self.base_image, 270)
+    elif self.face == 5:
+      self.image = pygame.transform.flip(self.base_image, True, False)
+      self.image = pygame.transform.rotate(self.image, 45)
+    elif self.face == 6:
+      self.image = pygame.transform.flip(self.base_image, True, False)
+    elif self.face == 7:
+      self.image = pygame.transform.flip(self.base_image, True, False)
+      self.image = pygame.transform.rotate(self.image, 315)
+    
     if self.health <= 0:
       die()
 
